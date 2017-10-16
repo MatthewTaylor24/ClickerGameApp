1d6027b316129e04ddb4daf5f5715dcdb274729f
Matthew Taylor <matcurtay@comcast.net>
2017-10-16 16:27:43 -0700

    Added Upgrade to Clicker
    Created a button that will upgrade the clicker if the user has enough Score to purchase it.

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -13,8 +13,9 @@ public class MainWindow {
 
 	protected Shell shell;
 	
-	public int userScore = 0;
+	public int userScore = 0, clickerBonus = 1, bonusLevel = 0;
 	private Text text;
+	private Text text_1;
 
 	/**
 	 * Launch the application.
@@ -52,18 +53,25 @@ public class MainWindow {
 		shell.setSize(510, 352);
 		shell.setText("SWT Application");
 		
+		// User Score Text
 		text = new Text(shell, SWT.BORDER);
-		text.setBounds(10, 10, 76, 21);
+		text.setBounds(71, 7, 76, 21);
 		
-		Button btnNewButton = new Button(shell, SWT.NONE);
-		btnNewButton.addMouseListener(new MouseAdapter() {
+		// Upgrade Clicker Button Text
+		text_1 = new Text(shell, SWT.BORDER);
+		text_1.setText("20");
+		text_1.setBounds(168, 241, 76, 21);
+		
+		// Clicker Button
+		Button clickerButton = new Button(shell, SWT.NONE);
+		clickerButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDown(MouseEvent e) {
-				userScore++;
+				userScore += clickerBonus;
 				text.setText("" + userScore);
 			}
 		});
-		btnNewButton.addSelectionListener(new SelectionAdapter() {
+		clickerButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				
@@ -71,8 +79,38 @@ public class MainWindow {
 			
 			
 		});
-		btnNewButton.setBounds(10, 105, 228, 49);
-		btnNewButton.setText("Add +1");
+		
+		clickerButton.setBounds(10, 182, 228, 49);
+		clickerButton.setText("Add +" + clickerBonus);
+		
+		Label lblUserScore = new Label(shell, SWT.NONE);
+		lblUserScore.setBounds(10, 10, 55, 15);
+		lblUserScore.setText("User Score:");
+		
+		// Upgrade Clicker Button
+		Button btnUpgradeClicker = new Button(shell, SWT.NONE);
+		btnUpgradeClicker.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				if (userScore >= Math.round((20+17*bonusLevel*Math.pow(bonusLevel, 1.59)))) {
+					clickerBonus *= 2;
+					clickerButton.setText("Add +" + clickerBonus);
+					userScore -= Math.round((20+17*bonusLevel*Math.pow(bonusLevel, 1.59)));
+					text.setText(""+userScore);
+					bonusLevel++;
+					text_1.setText("" + Math.round((20+17*bonusLevel*Math.pow(bonusLevel, 1.59))));
+				}
+			}
+		});
+		btnUpgradeClicker.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		btnUpgradeClicker.setBounds(10, 237, 152, 25);
+		btnUpgradeClicker.setText("Upgrade Clicker");
+		
+		
 		
 		
 
